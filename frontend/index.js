083c73b677c7b411c4b81fde59198e1cab1b207a
J0nathanMB <belmirojonathan2@gmail.com>
2024-11-18 17:28:57 -0300

    troca para zerotier

--- frontend/index.js
+++ frontend/index.js
@@ -2,7 +2,8 @@ const BG_COLOUR = '#231f20';
 const SNAKE_COLOUR = '#c2c2c2';
 const FOOD_COLOUR = '#e66916';
 
-const socket = io('https://localhost:3000');
+// const socket = io('http://localhost:3000');
+const socket = io('http://192.168.196.170:3000');
 
 socket.on('init', handleInit);
 socket.on('gameState', handleGameState);
