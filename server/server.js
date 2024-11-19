e7c04a1e0dc3dc3fb87df1c6345b4d7b5d978276
J0nathanMB <belmirojonathan2@gmail.com>
2024-11-18 21:24:09 -0300

    novo teste

--- server/server.js
+++ server/server.js
@@ -90,7 +90,13 @@ function startGameInterval(roomName) {
 }
 
 function emitGameState(room, gameState) {
-  io.to(room).emit('gameState', JSON.stringify(gameState));
+  const clients = Array.from(io.sockets.adapter.rooms.get(room) || []);
+  for (const clientId of clients) {
+    const client = io.of('/').sockets.get(clientId); // Substituído para versões recentes
+    if (client) {
+      client.emit('gameState', JSON.stringify(gameState));
+    }
+  }
 }
 
 function emitGameOver(room, winner) {
